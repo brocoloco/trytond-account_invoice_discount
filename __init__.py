a89707c9b8faf178a38f26dee06527fa79056778
Albert Cervera i Areny <albert@nan-tic.com>
2013-10-27 09:31:42 +0100

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+
+from trytond.pool import Pool
+from .invoice import *
+
+def register():
+    Pool.register(
+        InvoiceLine,
+        module='account_invoice_discount', type_='model')
