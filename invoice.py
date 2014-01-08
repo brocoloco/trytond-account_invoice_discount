44cb19d7f631461a8f54b2172583b5cece6e0f76
Albert Cervera i Areny <albert@nan-tic.com>
2014-01-09 00:22:28 +0100

    Avoid division by zero.

--- invoice.py
+++ invoice.py
@@ -44,7 +44,8 @@ class InvoiceLine:
             digits = self.__class__.unit_price.digits[1]
             unit_price = unit_price.quantize(Decimal(str(10.0 ** -digits)))
 
-            gross_unit_price = unit_price / (1 - self.discount)
+            if (1 - self.discount) != 0:
+                gross_unit_price = unit_price / (1 - self.discount)
             digits = self.__class__.gross_unit_price.digits[1]
             gross_unit_price = gross_unit_price.quantize(
                 Decimal(str(10.0 ** -digits)))
