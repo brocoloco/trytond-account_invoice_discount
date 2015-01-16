2b41090145b613c2051e3c32e31af23c53825e62
resteve <resteve@zikzakmedia.com>
2015-01-16 13:15:28 +0100

    Calculate gross unit price is 0 when unit price is None
    issue3

--- invoice.py
+++ invoice.py
@@ -83,7 +83,8 @@ class InvoiceLine:
         for vals in vlist:
             if vals.get('type') != 'line':
                 continue
-            gross_unit_price = vals.get('unit_price', Decimal('0.0'))
+            gross_unit_price = (vals.get('unit_price', Decimal('0.0'))
+                or Decimal('0.0'))
             if 'discount' in vals and vals['discount'] != 1:
                 gross_unit_price = gross_unit_price / (1 - vals['discount'])
                 digits = cls.gross_unit_price.digits[1]
