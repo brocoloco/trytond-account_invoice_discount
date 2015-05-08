0721ee406e6587d8e3a41cce3bae78b4e09eac8f
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-05-08 16:31:33 +0200

    Fix flake8 errors

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
-
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.pool import Pool
 from .invoice import *
+
 
 def register():
     Pool.register(
--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst', '*.odt']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst',
+                '*.odt']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
