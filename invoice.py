5c81306b02adf4213cdf1d01febe94437c4de505
Guillem Barba <guillem@nan-tic.com>
2015-06-23 15:04:01 +0200

    Invisible invoice_discount field when is not line

--- invoice.py
+++ invoice.py
@@ -25,13 +25,15 @@ class InvoiceLine:
     __name__ = 'account.invoice.line'
 
     gross_unit_price = fields.Numeric('Gross Price', digits=(16, DIGITS),
-        states=STATES)
+        states=STATES, depends=['type'])
     gross_unit_price_wo_round = fields.Numeric('Gross Price without rounding',
         digits=(16, DIGITS + DISCOUNT_DIGITS), readonly=True)
     discount = fields.Numeric('Discount', digits=(16, DISCOUNT_DIGITS),
-        states=STATES)
+        states=STATES, depends=['type'])
     invoice_discount = fields.Numeric('Invoice Discount',
-        digits=(16, DISCOUNT_DIGITS))
+        digits=(16, DISCOUNT_DIGITS), states={
+            'invisible': Eval('type') != 'line',
+            }, depends=['type'])
 
     @classmethod
     def __setup__(cls):
