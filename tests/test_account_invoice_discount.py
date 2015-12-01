e4db4665900f4b6adc0e6fd1ce2ffbdae6c29c56
resteve <resteve@zikzakmedia.com>
2015-12-01 10:56:57 +0100

    Tests

--- tests/test_account_invoice_discount.py
+++ tests/test_account_invoice_discount.py
@@ -1,34 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the account_invoice_discount module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceDiscountTestCase(unittest.TestCase):
+class AccountInvoiceDiscountTestCase(ModuleTestCase):
     'Test Account Invoice Discount module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_discount')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_invoice_discount')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_discount'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            AccountInvoiceDiscountTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_invoice.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+        AccountInvoiceDiscountTestCase))
     return suite
