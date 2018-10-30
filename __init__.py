2993be7ba9be5894a41e2167ef9916834efdeca5
Raimon Esteve <raimon@nan-tic.com>
2018-10-30 10:23:29 +0100

    Set gross unit price when create an invoice line from commission #035839

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from . import commission
 from . import invoice
-
 
 def register():
     Pool.register(
         invoice.InvoiceLine,
+        module='account_invoice_discount', type_='model')
+    Pool.register(
+        commission.Commission,
+        depends=['commission'],
         module='account_invoice_discount', type_='model')
