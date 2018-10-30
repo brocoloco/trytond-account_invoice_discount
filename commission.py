95d34b8d6f77c6e1294e51057df6733e38c4b7d6
Raimon Esteve <raimon@nan-tic.com>
2018-10-30 10:43:57 +0100

    Set gross unit price when create an invoice line from commission #035839

---
+++ commission.py
@@ -0,0 +1,19 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from decimal import Decimal
+from trytond.pool import PoolMeta
+
+__all__ = ['Commission']
+
+
+class Commission(metaclass=PoolMeta):
+    __name__ = 'commission'
+
+    @classmethod
+    def _get_invoice_line(cls, key, invoice, commissions):
+        invoice_line = super(Commission, cls)._get_invoice_line(
+            key, invoice, commissions)
+        # set gross_unit_price from unit_price
+        invoice_line.gross_unit_price = invoice_line.unit_price
+        invoice_line.discount = Decimal(0)
+        return invoice_line
